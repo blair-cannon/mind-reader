a1f49565c67519c2894c30aaa7fe08e164b0958e
Blair Preston <blair.preston@student.awesomeinc.org>
2022-02-24 16:54:36 +0000

    update symbols array

--- js/myscripts.js
+++ js/myscripts.js
@@ -5,7 +5,7 @@ const symbolsArray = nineSymbolsArray.concat(nineSymbolsArray.concat(nineSymbols
 const headerTextArr = [ "I can read your mind...", "Pick a number from 01 - 99", "Add both digits together to get a new number", "Subtract your new number from the original number", symbolsArray, symbolsArray[0]];
 const circleTextArr = [ "GO", "Restart"]
 const rectTextArr = [ "NEXT", "REVEAL"]
-const helperTextArr = [ "when you have your number click next", "Ex: 14 is 1 + 4 = 5 click next to proceed", "Ex: 14 - 5 = 9 click next to proceed", "Find your new number. Note the symbol beside the number.", "Your symbol is: TODOOO"];
+const helperTextArr = [ "when you have your number click next", "Ex: 14 is 1 + 4 = 5 click next to proceed", "Ex: 14 - 5 = 9 click next to proceed", "Find your new number. Note the symbol beside the number.", "Your symbol is: " + symbolsArray[0]];
 
 
 // create parent class 
